3ae7aa4aba81829322ccc255cbb444f50f111dda
reneavalosdeveloper <svc@atlantics.mx>
2025-02-06 14:42:50 -0600

    Fix mysql connections

--- db_config.py
+++ db_config.py
@@ -3,9 +3,12 @@ import mysql.connector
 # Función para conectar a la base de datos MySQL
 def connect_db():
       return mysql.connector.connect(
-        host="localhost",
+        host="46.202.198.207",
+        user="u723373843_rene",  # Cambia esto por tu usuario
+        password="Atlantics01!",  # Cambia esto por tu contraseña
+        database="u723373843_DB_WEB_ATLANTI"
+    )
+'''  host="localhost",
         user="Atlantics",  # Cambia esto por tu usuario
         password="Atl@ntics02!",  # Cambia esto por tu contraseña
-        database="chatbot_db"
-    )
-      
+        database="chatbot_db" '''
