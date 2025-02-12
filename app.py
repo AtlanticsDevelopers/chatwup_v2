2b894962454635f8d172d0b9097c4996108e0a8c
reneavalosdeveloper <svc@atlantics.mx>
2025-02-12 11:04:27 -0600

    New changes adding whatsapp logs 13

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 import requests
 import os
+import uvicorn
 from pydantic import BaseModel
 from chatbot import ask_question
 
@@ -10,6 +11,12 @@ app = FastAPI()
 # Modelo de datos para recibir preguntas
 class Question(BaseModel):
     question: str
+    
+port = int(os.environ.get("PORT", 10000))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=port)
+    
 ##IS AVAILABLE
 @app.get("/health")
 async def health_check():
